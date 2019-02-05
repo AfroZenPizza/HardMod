1faebd883ada77d37d4931fa8337595e9bcecdf5
Gary Welch <welch.gary@gmail.com>
2019-02-04 20:56:03 -0600

    feat: Clay Mound Block
    Clay Mound Block added for example purposes.
    Diff previous and this to see how to add a block.

--- src/main/java/com/tsumuku/hardmod/init/ModBlocks.java
+++ src/main/java/com/tsumuku/hardmod/init/ModBlocks.java
@@ -14,5 +14,5 @@ public class ModBlocks
 	public static final List<Block> BLOCKS = new ArrayList<Block>();
 	
 	public static final Block MUD_BLOCK = new BlockBase("mud_block", Material.CLAY);
-	
+	public static final Block CLAY_MOUND_BLOCK = new BlockBase("clay_mound_block", Material.CLAY);
 }
