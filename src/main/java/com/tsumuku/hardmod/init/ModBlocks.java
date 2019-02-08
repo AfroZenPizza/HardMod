567c122061916b51f9ed95e7ad162a2e4b80fd7c
Gary Welch <welch.gary@gmail.com>
2019-02-08 00:51:19 -0600

    func: added Wool Torch

--- src/main/java/com/tsumuku/hardmod/init/ModBlocks.java
+++ src/main/java/com/tsumuku/hardmod/init/ModBlocks.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import com.tsumuku.hardmod.blocks.BlockBase;
 import com.tsumuku.hardmod.blocks.MudBlock;
+import com.tsumuku.hardmod.blocks.WoolTorchBlock;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
@@ -16,4 +17,6 @@ public class ModBlocks
 	
 	public static final Block MUD_BLOCK = new MudBlock("mud_block", Material.CLAY);
 	public static final Block CLAY_MOUND_BLOCK = new BlockBase("clay_mound_block", Material.CLAY);
+	public static final Block WOOL_TORCH = new WoolTorchBlock("wool_torch_block");
+	
 }
