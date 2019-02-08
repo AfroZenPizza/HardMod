45e30050aeb10903fe8621fd713973679c90de55
Gary Welch <welch.gary@gmail.com>
2019-02-08 03:31:29 -0600

    feat: Added Seed Oil

--- src/main/java/com/tsumuku/hardmod/init/ModItems.java
+++ src/main/java/com/tsumuku/hardmod/init/ModItems.java
@@ -14,5 +14,6 @@ public class ModItems
 
 	public static final List<Item> ITEMS = new ArrayList<Item>();
 	public static final Item WOOL_CLOTH = new ItemBase( "wool_cloth" );
+	public static final Item SEED_OIL = new ItemBase( "seed_oil" );
 	
 }
